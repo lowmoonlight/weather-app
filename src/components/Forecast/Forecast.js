4aaf924858140f9d9e4f548e73c28c9ca519da42
Kim Blott <kimberlyanneblott@gmail.com>
2020-07-05 15:52:36 -0400

    React weather app based on tutorial with modifications

---
+++ src/components/Forecast/Forecast.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Form from "../Form/Form";
+
+const Forecast = (props) => {
+  return (
+    <div>
+      <h2>Find Current Weather Conditions</h2>
+      <Form />
+    </div>
+  );
+};
+
+export default Forecast;
